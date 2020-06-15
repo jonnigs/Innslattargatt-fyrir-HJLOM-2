ba34392572f0a970ae98d0099b392835ab86df52
jonnigs <jonnigs@gmail.com>
2020-06-15 16:45:29 +0000

    Lok dags
    Fékk dynamic event listerner til að virka

--- repeat.js
+++ repeat.js
@@ -1,19 +1,31 @@
 var fjoldi = document.getElementsByClassName("fjoldi").item(0);
 var harmonikka = document.getElementsByClassName("harmonikkuDiv").item(0);
+var pila;
 
 document;
 fjoldi.addEventListener("input", (e) => {
   empty(harmonikka);
   for (i = 1; i <= fjoldi.value; i++) {
     var nemandi = document.createElement("div");
-    nemandi.className = "nemandi" + i;
+    nemandi.className = "nemandi " + i;
+
+    pila = document.createElement("img");
+    pila.className = "closed";
+    pila.src = "./img/arrow-left.svg";
+    pila.alt = "Ör til vinstri";
+    nemandi.appendChild(pila);
 
     var p = document.createElement("p");
-    p.textContent = "Eitthvað " + i + " Kennitala: 000000-0000";
+    p.textContent =
+      "Nemandi: Gunnar Gunnarsson " + i + " Kennitala: 000000-0000";
+    nemandi.appendChild(p);
 
-    nemandi.appendChild(p);
-    p.textContent = "Eitthvað annað";
-    nemandi.appendChild(p);
     harmonikka.appendChild(nemandi);
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target && e.target.className == "closed") {
+    console.log("hér");
   }
 });
